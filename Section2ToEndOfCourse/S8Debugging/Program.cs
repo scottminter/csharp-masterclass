4d477770419c4f9fd3ef315022facf7fc6b96699
Scott Minter <sminter@gmail.com>
2023-08-17 14:25:04 -0400

    start of debugging section

---
+++ Section2ToEndOfCourse/S8Debugging/Program.cs
@@ -0,0 +1,8 @@
+﻿using S8Debugging;
+
+Console.WriteLine("Section 8: Debugging");
+V123DebuggingBasics.Lesson();
+
+
+
+Console.Read();
---
+++ Section2ToEndOfCourse/S8Debugging/V123DebuggingBasics.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace S8Debugging
+{
+    public class V123DebuggingBasics
+    {
+        public static void Lesson()
+        {
+            var friends = new List<string> { "Frank", "Joe", "Michelle", "Al", "J", "Andy", "Maria", "Carlos", "Angelica" };
+            var partyFriends = GetPartyFriends(friends, 4);
+
+            foreach (var name in partyFriends)
+            {
+                Console.WriteLine($"name: {name}");
+            }
+        }
+
+        public static List<string> GetPartyFriends(List<string> list, int count)
+        {
+            var partyFriends = new List<string>();
+
+            while (partyFriends.Count < count)
+            {
+                var currentFriend = GetPartyFriend(list);
+                partyFriends.Add(currentFriend);
+                list.Remove(currentFriend);
+            }
+
+            return partyFriends;
+        }
+
+        public static string GetPartyFriend(List<string> list)
+        {
+            string shortestName = list[0];
+            for (var i = 0; i < list.Count; i++)
+            {
+                if (list[i].Length < shortestName.Length)
+                {
+                    shortestName = list[i];
+                }
+            }
+
+            return shortestName;
+        }
+    }
+}
+
